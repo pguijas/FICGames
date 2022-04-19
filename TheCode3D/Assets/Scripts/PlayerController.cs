88df71450bfffbfdbb1c5165351215290eafaa4f
the-scripting-boy-4040 <hectorpadint00@gmail.com>
2022-04-19 19:43:28 +0200

    fps controler basico

---
+++ TheCode3D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+    
+    public float walkSpeed = 5f;
+    public float runSpeed = 20f;
+    public float jumpHeight = 10f;
+    public float gravityScale = -20f;
+
+    Vector3 moveInput = Vector3.zero;
+    CharacterController characterController;
+
+    private void Awake() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        Move();
+    }
+
+    private void Move() {
+        if (characterController.isGrounded) {
+            moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            moveInput = Vector3.ClampMagnitude(moveInput, 1f);
+            //if (Input.GetButtonDown("Sprint"))
+            //    moveInput = transform.TransformDirection(moveInput) * runSpeed;
+            //else
+                moveInput = transform.TransformDirection(moveInput) * walkSpeed;
+            if (Input.GetButtonDown("Jump"))
+                moveInput.y = Mathf.Sqrt(jumpHeight * -2f * gravityScale);
+        }
+        moveInput.y += gravityScale * Time.deltaTime;
+	    characterController.Move(moveInput * Time.deltaTime);
+    }
+}
