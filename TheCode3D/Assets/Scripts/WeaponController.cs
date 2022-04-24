4245e76d2289a698d0c94630b68fec727219cda5
the-scripting-boy-4040 <hectorpadint00@gmail.com>
2022-04-24 23:26:42 +0200

    animación flash

--- TheCode3D/Assets/Scripts/WeaponController.cs
+++ TheCode3D/Assets/Scripts/WeaponController.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponController : MonoBehaviour {
+public class WeaponController : MonoBehaviour
+{
+    [Header("References")]
+    public Transform weaponMuzzle;
 
     [Header("General")]
     public LayerMask hittableLayers;
@@ -10,25 +13,38 @@ public class WeaponController : MonoBehaviour {
 
     [Header("Shoot Paramaters")]
     public float fireRange = 200;
+    public float recoilForce = 4f; //Fuerza de retroceso del arma
+
+    [Header("Sounds & Visuals")]
+    public GameObject flashEffect;
 
     private Transform cameraPlayerTransform;
 
-    void Start() {
+    private void Start()
+    {
         cameraPlayerTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
     }
 
-    void Update(){
+    private void Update()
+    {
         HandleShoot();
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, Vector3.zero, Time.deltaTime * 5f);
     }
 
-    private void HandleShoot(){
-        if (Input.GetButtonDown("Fire1")){
-            Debug.Log("Shoot");
+    private void HandleShoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject flashClone = Instantiate(flashEffect, weaponMuzzle.position, Quaternion.Euler(weaponMuzzle.forward), transform);
+            Destroy(flashClone, 1f);
+
             RaycastHit hit;
-            if (Physics.Raycast(cameraPlayerTransform.position, cameraPlayerTransform.forward, out hit, fireRange, hittableLayers)){
+            if (Physics.Raycast(cameraPlayerTransform.position, cameraPlayerTransform.forward, out hit, fireRange, hittableLayers))
+            {
                 GameObject bulletHoleClone = Instantiate(bulletHolePrefab, hit.point + hit.normal * 0.001f, Quaternion.LookRotation(hit.normal));
                 Destroy(bulletHoleClone, 4f);
             } 
         }
     }
-}
+}   
